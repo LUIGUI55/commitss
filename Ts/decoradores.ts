c14f954ed54f602b13eaa83cac3f3a27a651bb30
LUIGUI55 <luisantonio6942@gmail.com>
2025-03-05 05:47:35 -0600

    ts

---
+++ Ts/decoradores.ts
@@ -0,0 +1,10 @@
+function logConstructor(target: Function) {
+    console.log(`Constructor de ${target.name} llamado`);
+}
+
+@logConstructor
+class Vehiculo {
+    constructor(public marca: string) {}
+}
+
+let coche = new Vehiculo("Toyota");
---
+++ Ts/decoradoresPropieda.ts
@@ -0,0 +1,33 @@
+function logProperty(target: any, key: string) {
+    let value = target[key];
+
+    const getter = () => {
+        console.log(`Get: ${key} => ${value}`);
+        return value;
+    };
+
+    const setter = (newVal) => {
+        console.log(`Set: ${key} => ${newVal}`);
+        value = newVal;
+    };
+
+    Object.defineProperty(target, key, {
+        get: getter,
+        set: setter,
+        enumerable: true,
+        configurable: true
+    });
+}
+
+class Persona {
+    @logProperty
+    nombre: string;
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+    }
+}
+
+let persona = new Persona("Luis");
+persona.nombre = "Carlos";
+console.log(persona.nombre);
